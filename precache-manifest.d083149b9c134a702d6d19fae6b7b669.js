b510f7b146dfbb84045a13b781a6cd752bef8132
tulbug <tulgabugra@gmail.com>
2018-12-14 01:19:49 +0300

    Updates

---
+++ precache-manifest.d083149b9c134a702d6d19fae6b7b669.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbca9f80a194cf1f312b",
+    "url": "/catfriends/static/css/main.b33b400e.chunk.css"
+  },
+  {
+    "revision": "dbca9f80a194cf1f312b",
+    "url": "/catfriends/static/js/main.dbca9f80.chunk.js"
+  },
+  {
+    "revision": "1bdb72ccf9ba1b9004b6",
+    "url": "/catfriends/static/css/1.54acea5e.chunk.css"
+  },
+  {
+    "revision": "1bdb72ccf9ba1b9004b6",
+    "url": "/catfriends/static/js/1.1bdb72cc.chunk.js"
+  },
+  {
+    "revision": "e77e3cefe0b2c17ce573",
+    "url": "/catfriends/static/js/runtime~main.e77e3cef.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/catfriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "9e313c58923692e5afe59dfe50e0bf84",
+    "url": "/catfriends/index.html"
+  }
+];
